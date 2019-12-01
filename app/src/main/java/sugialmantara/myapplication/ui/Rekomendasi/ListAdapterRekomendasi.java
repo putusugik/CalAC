b91556a9c082856713eeb23513cd33be332f9bc5
I Putu Sugi Almantara <sugi.almantara@yahoo.co.id>
2019-12-01 21:04:20 +0800

    Tugas Pemrograman Update 3

--- app/src/main/java/sugialmantara/myapplication/ui/Rekomendasi/ListAdapterRekomendasi.java
+++ app/src/main/java/sugialmantara/myapplication/ui/Rekomendasi/ListAdapterRekomendasi.java
@@ -2,71 +2,62 @@ package sugialmantara.myapplication.ui.Rekomendasi;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import sugialmantara.myapplication.R;
 
 public class ListAdapterRekomendasi extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context context;
-
+    List<DataACRekomen> data = Collections.emptyList();
     int temp;
-
-
-    private ArrayList<HashMap<String,String>> data;
-    private ArrayList<HashMap<String,String>> dataBackup;
     private static LayoutInflater inflater = null;
-    HashMap<String,String> booking = new HashMap<String, String>();
 
 
-    public ListAdapterRekomendasi(Context context, ArrayList<HashMap<String, String>> dataACS){
+
+    public ListAdapterRekomendasi(Context context, List<DataACRekomen> dataACS){
         this.context = context;
         inflater =LayoutInflater.from(context);
         this.data = dataACS;
         inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
+
     @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    /*@NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = inflater.from(parent.getContext()).inflate(R.layout.list_acrekomen, parent, false);
         ListViewHolder holder = new ListViewHolder(view);
 
         return holder;
-    }*/
+    }
 
-    /*@Override
+    @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         //((ListViewHolder) holder ).bindView(position);
         DecimalFormat decim = new DecimalFormat("#,###.##");
         final ListViewHolder holder1 =(ListViewHolder) holder;
-        final DataACRekomen dataAC =data.get(position);
-        holder1.tvnama.setText(dataAC.Nama_ACRekomen);
-        holder1.tvmerk.setText(dataAC.Merk_ACRekomen);
-        holder1.tvtipe.setText(dataAC.Tipe_ACRekomen);
-        temp = Integer.parseInt(dataAC.Harga_ACRekomen);
+        final DataACRekomen dataACRekomen = data.get(position);
+        holder1.tvnama.setText(dataACRekomen.Nama_ACRekomen);
+        holder1.tvmerk.setText(dataACRekomen.Merk_ACRekomen);
+        holder1.tvtipe.setText(dataACRekomen.Tipe_ACRekomen);
+        temp = Integer.parseInt(dataACRekomen.Harga_ACRekomen);
         holder1.tvharga.setText("Rp. "+decim.format(temp));
 
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
     }
 
     @Override
@@ -89,6 +80,6 @@ public class ListAdapterRekomendasi extends RecyclerView.Adapter<RecyclerView.Vi
 
         }
 
-    }*/
+    }
 
 }
--- app/src/main/java/sugialmantara/myapplication/ui/Rekomendasi/RekomendasiACFragment.java
+++ app/src/main/java/sugialmantara/myapplication/ui/Rekomendasi/RekomendasiACFragment.java
@@ -1,17 +1,30 @@
 package sugialmantara.myapplication.ui.Rekomendasi;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -19,30 +32,19 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import cz.msebera.android.httpclient.NameValuePair;
-import cz.msebera.android.httpclient.message.BasicNameValuePair;
-import sugialmantara.myapplication.JSONParser;
 import sugialmantara.myapplication.R;
 
 public class RekomendasiACFragment extends Fragment {
 
-    public static final String TAG_SUCCESS = "sukses";
-    public static final String TAG_NAMAAC = "nama_ac";
-    public static final String TAG_MERKAC = "merk_ac";
-    public static final String TAG_TIPEAC = "tipe_ac";
-    public final String TAG_HARGAAC = "harga";
-    public static final String TAG_INDEX = "index";
-    JSONParser js = new JSONParser();
     private RecyclerView recyclerView;
-    JSONParser jsonParser = new JSONParser();
+    TextView textView;
     public ListAdapterRekomendasi listAdapter;
     List<DataACRekomen> data = new ArrayList<>();
     private static String url_viewAC = "http://arismart.co.id/sugiklib/select_rekomendasi.php";
     public static final int CONN_TIMEOUT = 10000;
     public static final int CONN_READOUT = 15000;
     int hasil;
-    JSONArray rekomen = null;
-    ArrayList<HashMap<String, String>> listrek = new ArrayList<HashMap<String, String>>();
+
 
     public RekomendasiACFragment(){
 
@@ -54,59 +56,99 @@ public class RekomendasiACFragment extends Fragment {
         hasil = getArguments().getInt("hasil", -1);
         new viewRekomen().execute();
 
-
-
         return root[0];
         }
 
-    private class viewRekomen extends AsyncTask<String,String, String >{
 
+    private class viewRekomen extends AsyncTask<String, String, String>{
+        HttpURLConnection conn;
+        URL url = null;
 
         @Override
         protected String doInBackground(String... strings) {
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("hasil", ""+hasil));
-
-            JSONObject object = jsonParser.makeHttpRequest(url_viewAC, "GET", params);
-            Log.d("Back: ", object.toString());
-
-            try {
-                int sukses = object.getInt(TAG_SUCCESS);
-                if (sukses==1){
-                    rekomen = object.getJSONArray("AC");
-                    listrek.removeAll(listrek);
-                    for (int i=0;i<rekomen.length();i++){
-                        JSONObject obj = rekomen.getJSONObject(i);
-                        HashMap<String, String> map = new HashMap<String, String>();
-                        String namaAC = obj.getString(TAG_NAMAAC);
-                        String merkAC = obj.getString(TAG_MERKAC);
-                        String tipeAC = obj.getString(TAG_TIPEAC);
-                        String hargaAC = obj.getString(TAG_HARGAAC);
-
-                        map.put(TAG_NAMAAC, namaAC);
-                        map.put(TAG_MERKAC, merkAC);
-                        map.put(TAG_TIPEAC, tipeAC);
-                        map.put(TAG_HARGAAC, hargaAC);
-                        map.put(TAG_INDEX, ""+i);
-                        listrek.add(map);
-                    }
-                }
-            } catch (Exception e) {
+            try{
+                url = new URL(url_viewAC);
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
+                return e.toString();
             }
+            try{
+                conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(CONN_READOUT);
+                conn.setConnectTimeout(CONN_TIMEOUT);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                Uri.Builder builder = new Uri.Builder().appendQueryParameter("hasil", String.valueOf(hasil));
+                String query = builder.build().getEncodedQuery();
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
 
-            return null;
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                return e1.toString();
+            }
+            try{
+                int response_code = conn.getResponseCode();
+                if(response_code == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine())!=null){
+                        result.append(line);
+                    }
+                    return (result.toString());
+                }else {
+                    return ("tidak sukses");
+                }
+
+            } catch (IOException e2) {
+                e2.printStackTrace();
+                return e2.toString();
+            }finally {
+                conn.disconnect();
+            }
         }
 
         @Override
         protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            recyclerView = (RecyclerView)getActivity().findViewById(R.id.rlListACRekomen);
-            listAdapter = new ListAdapterRekomendasi(getActivity(), listrek);
-            recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
-            recyclerView.setAdapter(listAdapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        }
+            try {
+                JSONObject object = new JSONObject(s);
+                JSONArray jsonArray = object.getJSONArray("AC");
+                Log.d("JSONAAAAA: ",jsonArray.toString());
+                for (int i = 0; i < jsonArray.length();i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    DataACRekomen dataACRekomen = new DataACRekomen();
+                    dataACRekomen.Nama_ACRekomen=jsonObject.getString("nama_ac");
+                    dataACRekomen.Merk_ACRekomen=jsonObject.getString("merk_ac");
+                    dataACRekomen.Tipe_ACRekomen=jsonObject.getString("tipe_ac");
+                    dataACRekomen.Harga_ACRekomen=jsonObject.getString("harga");
+                    data.add(dataACRekomen);
+                    Log.d("DATA: ", data.toString());
+                }
 
+                recyclerView = (RecyclerView)getActivity().findViewById(R.id.rlListACRekomen);
+                if (data.isEmpty()){
+                    textView = (TextView) getActivity().findViewById(R.id.textView3);
+                    Toast.makeText(getActivity(), "Tidak ada yang cocok", Toast.LENGTH_SHORT).show();
+                }else {
+                    listAdapter = new ListAdapterRekomendasi(getActivity(), data);
+                    recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+                    recyclerView.setAdapter(listAdapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
